542d03a3897761fa57f3b853d43f272fbef29ad2
benting180 <benting180@gmail.com>
2020-10-17 16:53:26 +0800

    ch10 generalize pattern

--- pattern.py
+++ pattern.py
@@ -1,21 +1,14 @@
 from math import floor
 from matrix import Matrix
+from color import Color
 
-class StripePattern:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+class Pattern:
+    def __init__(self):
         self.transform = Matrix([[1, 0, 0, 0],
                                [0, 1, 0, 0],
                                [0, 0, 1, 0],
                                [0, 0, 0, 1]])
 
-    def stripe_at(self, point):
-        if floor(point.x) % 2 == 0:
-            return self.a
-        else:
-            return self.b
-    
     def set_pattern_transform(self, transform):
         self.transform = transform
     
@@ -23,3 +16,24 @@ class StripePattern:
         object_point = obj.transform.inverse() * world_point
         pattern_point = self.transform.inverse() * object_point
         return self.stripe_at(pattern_point)
+
+
+class StripePattern(Pattern):
+    def __init__(self, a, b):
+        Pattern.__init__(self)
+        self.a = a
+        self.b = b
+        
+    def stripe_at(self, point):
+        if floor(point.x) % 2 == 0:
+            return self.a
+        else:
+            return self.b
+
+
+class TestPattern(Pattern):
+    def __init__(self):
+        Pattern.__init__(self)
+
+    def stripe_at(self, point):
+        return Color(point.x, point.y, point.z)
--- test/test_pattern.py
+++ test/test_pattern.py
@@ -8,12 +8,53 @@ from ray import Ray
 from math import sqrt, pi
 from material import Material
 from shape import Shape, TestShape
-from pattern import StripePattern
+from pattern import StripePattern, TestPattern
 from color import Color
 from sphere import Sphere
+import matrix
+
+class TestPatternClass(unittest.TestCase):
+    def test_default(self):
+        p = TestPattern()
+        I = Matrix([[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+        self.assertTrue(matrix.equals(I, p.transform))
 
 
-class TestPattern(unittest.TestCase):
+
+    def test_transform1(self):
+        p = TestPattern()
+        p.set_pattern_transform(translate(1, 2, 3))
+        self.assertTrue(matrix.equals(p.transform, translate(1, 2, 3)))
+
+
+    def test_pattern_with_an_object_transformation(self):
+        s = Sphere()
+        s.set_transform(scale(2, 2, 2))
+        p = TestPattern()
+        c = p.stripe_at_object(s, Point(2, 3, 4))
+        self.assertTrue(c.equals(Color(1, 1.5, 2)))
+
+    def test_pattern_with_a_pattern_transformation(self):
+        s = Sphere()
+        p = TestPattern()
+        p.set_pattern_transform(scale(2, 2, 2))
+        c = p.stripe_at_object(s, Point(2, 3, 4))
+        self.assertTrue(c.equals(Color(1, 1.5, 2)))
+
+    def test_pattern_with_both_an_object_and_a_pattern_transformation(self):
+        s = Sphere()
+        s.set_transform(scale(2, 2, 2))
+        p = TestPattern()
+        p.set_pattern_transform(translate(0.5, 1, 1.5))
+        c = p.stripe_at_object(s, Point(2.5, 3, 3.5))
+        self.assertTrue(c.equals(Color(0.75, 0.5, 0.25)))
+
+
+
+class TestStripePattern(unittest.TestCase):
     def test_creating_a_stripe_pattern(self):
         black = Color(0, 0, 0)
         white = Color(1, 1, 1)
